9fce3e22a1ec9001ac98e8b682ed1cf12da56622
klavssprugevics <klavs.sprugevics@gmail.com>
2023-08-23 20:11:42 +0300

    Refactor

--- bingo.js
+++ bingo.js
@@ -1,166 +1,106 @@
-const CARDS = [
-    "Someone gets a pentakill",
-    "Racial slurs",
-    "GG EZ",
-    "Something in polish",
-    "JG DIFF",
-    "Baron",
-    "Dragon steal",
-    "Red/blue buff steal",
-    "Level 7 mastery",
-    "100k+ mastery points",
-    "Everyone in a team is Level 5+ mastery",
-    "Everyone has a skin",
-    "Skin from the newest skin-line",
-    "Yasuo/Yone level 7",
-    "Threatening to AFK",
-    "'Dont worry guys, I can carry'",
-    "'Lets FF' / FF15"
-];
-
-let won = false;
-
-// https://stackoverflow.com/a/46545530
-function shuffleCards() {
-    let shuffledCards = CARDS
-        .map(value => ({value, sort: Math.random()}))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({value}) => value)
-    return shuffledCards;
-}
-
-function setupCards() {
-    let cards = shuffleCards();
-    $(".bingoButton").each(function(i, obj) {
-        $(this).html(cards[i]);
-        $(this).removeClass("bingoButtonCheck");
-        $(this).removeClass("bigngoButtonBlink");
-    });
-}
-
-function checkForWin() {
-    if ($(".sq11").hasClass("bingoButtonCheck") &&
-        $(".sq12").hasClass("bingoButtonCheck") &&
-        $(".sq13").hasClass("bingoButtonCheck") &&
-        $(".sq14").hasClass("bingoButtonCheck"))
-    {
-        $(".sq11").addClass("bigngoButtonBlink");
-        $(".sq12").addClass("bigngoButtonBlink");
-        $(".sq13").addClass("bigngoButtonBlink");
-        $(".sq14").addClass("bigngoButtonBlink");
-        return true;
-    } 
-    if ($(".sq21").hasClass("bingoButtonCheck") &&
-        $(".sq22").hasClass("bingoButtonCheck") &&
-        $(".sq23").hasClass("bingoButtonCheck") &&
-        $(".sq24").hasClass("bingoButtonCheck"))
-    {
-        $(".sq21").addClass("bigngoButtonBlink");
-        $(".sq22").addClass("bigngoButtonBlink");
-        $(".sq23").addClass("bigngoButtonBlink");
-        $(".sq24").addClass("bigngoButtonBlink");
-        return true;
-    } 
-    if ($(".sq31").hasClass("bingoButtonCheck") &&
-        $(".sq32").hasClass("bingoButtonCheck") &&
-        $(".sq33").hasClass("bingoButtonCheck") &&
-        $(".sq34").hasClass("bingoButtonCheck"))
-    {
-        $(".sq31").addClass("bigngoButtonBlink");
-        $(".sq32").addClass("bigngoButtonBlink");
-        $(".sq33").addClass("bigngoButtonBlink");
-        $(".sq34").addClass("bigngoButtonBlink");
-        return true;
-    } 
-    if ($(".sq41").hasClass("bingoButtonCheck") &&
-        $(".sq42").hasClass("bingoButtonCheck") &&
-        $(".sq43").hasClass("bingoButtonCheck") &&
-        $(".sq44").hasClass("bingoButtonCheck"))
-    {
-        $(".sq41").addClass("bigngoButtonBlink");
-        $(".sq42").addClass("bigngoButtonBlink");
-        $(".sq43").addClass("bigngoButtonBlink");
-        $(".sq44").addClass("bigngoButtonBlink");
-        return true;
-    } 
-    if ($(".sq11").hasClass("bingoButtonCheck") &&
-        $(".sq21").hasClass("bingoButtonCheck") &&
-        $(".sq31").hasClass("bingoButtonCheck") &&
-        $(".sq41").hasClass("bingoButtonCheck"))
-    {
-        $(".sq11").addClass("bigngoButtonBlink");
-        $(".sq21").addClass("bigngoButtonBlink");
-        $(".sq31").addClass("bigngoButtonBlink");
-        $(".sq41").addClass("bigngoButtonBlink");
-        return true;
-    } 
-    if ($(".sq12").hasClass("bingoButtonCheck") &&
-        $(".sq22").hasClass("bingoButtonCheck") &&
-        $(".sq32").hasClass("bingoButtonCheck") &&
-        $(".sq32").hasClass("bingoButtonCheck"))
-    {
-        $(".sq12").addClass("bigngoButtonBlink");
-        $(".sq22").addClass("bigngoButtonBlink");
-        $(".sq32").addClass("bigngoButtonBlink");
-        $(".sq42").addClass("bigngoButtonBlink");
-        return true;
-    }
-    if ($(".sq13").hasClass("bingoButtonCheck") &&
-        $(".sq23").hasClass("bingoButtonCheck") &&
-        $(".sq33").hasClass("bingoButtonCheck") &&
-        $(".sq43").hasClass("bingoButtonCheck"))
-    {
-        $(".sq13").addClass("bigngoButtonBlink");
-        $(".sq23").addClass("bigngoButtonBlink");
-        $(".sq33").addClass("bigngoButtonBlink");
-        $(".sq43").addClass("bigngoButtonBlink");
-        return true;
-    }
-    if ($(".sq14").hasClass("bingoButtonCheck") &&
-        $(".sq24").hasClass("bingoButtonCheck") &&
-        $(".sq34").hasClass("bingoButtonCheck") &&
-        $(".sq44").hasClass("bingoButtonCheck"))
-    {
-        $(".sq14").addClass("bigngoButtonBlink");
-        $(".sq24").addClass("bigngoButtonBlink");
-        $(".sq34").addClass("bigngoButtonBlink");
-        $(".sq44").addClass("bigngoButtonBlink");
-        return true;
-    }
-    if ($(".sq11").hasClass("bingoButtonCheck") &&
-        $(".sq22").hasClass("bingoButtonCheck") &&
-        $(".sq33").hasClass("bingoButtonCheck") &&
-        $(".sq44").hasClass("bingoButtonCheck"))
-    {
-        $(".sq11").addClass("bigngoButtonBlink");
-        $(".sq22").addClass("bigngoButtonBlink");
-        $(".sq33").addClass("bigngoButtonBlink");
-        $(".sq44").addClass("bigngoButtonBlink");
-        return true;
-    } 
-    if ($(".sq41").hasClass("bingoButtonCheck") &&
-        $(".sq32").hasClass("bingoButtonCheck") &&
-        $(".sq23").hasClass("bingoButtonCheck") &&
-        $(".sq14").hasClass("bingoButtonCheck"))
-    {
-        $(".sq41").addClass("bigngoButtonBlink");
-        $(".sq32").addClass("bigngoButtonBlink");
-        $(".sq23").addClass("bigngoButtonBlink");
-        $(".sq14").addClass("bigngoButtonBlink");
-        return true;
-    } 
-}
-
 $(document).ready(function() {
-    setupCards();
-});
+    const CARDS = [
+        "Someone gets a pentakill",
+        "Racial slurs",
+        "GG EZ",
+        "Something in polish",
+        "JG DIFF",
+        "Baron",
+        "Dragon steal",
+        "Red/blue buff steal",
+        "Level 7 mastery",
+        "100k+ mastery points",
+        "Everyone in a team is Level 5+ mastery",
+        "Everyone has a skin",
+        "Skin from the newest skin-line",
+        "Yasuo/Yone level 7",
+        "Threatening to AFK",
+        "'Dont worry guys, I can carry'",
+        "'Lets FF' / FF15",
+        "Accusing someone of feeding",
+        "Complaining about an op champ",
+        "Complaining about an op item",
+        "Poro explodes in ARAM",
+        "Complaining about a summoner spell"
+    ];
 
-$("button").click(function() {
-    $(this).toggleClass("bingoButtonCheck");
-    if (checkForWin()) {
-        if (!won) {
-            alert("You win!")
-        }
-        won = true;
+    let boardState = {}
+    let rowCount = [0, 0, 0, 0];
+    let colCount = [0, 0, 0, 0];
+    let diag1Count = 0;
+    let diag2Count = 0;
+    setupCards();
+
+
+    function setupCards() {
+        let cards = shuffleCards();
+        $(".bingoButton").each(function(i, obj) {
+            $(this).html(cards[i]);
+            $(this).removeClass("bingoButtonCheck");
+            $(this).removeClass("bingoButtonBlink");
+        });
+        boardState = {}
+        rowCount = [0, 0, 0, 0];
+        colCount = [0, 0, 0, 0];
+        diag1Count = 0;
+        diag2Count = 0;
     }
+
+
+    // https://stackoverflow.com/a/46545530
+    function shuffleCards() {
+        let shuffledCards = CARDS
+            .map(value => ({value, sort: Math.random()}))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({value}) => value)
+        return shuffledCards;
+    }
+   
+
+    function updateGameState(buttonId) {
+        const button = $('#' + buttonId);
+        const [row, col] = buttonId.slice(2).split('').map(Number);
+        
+        if (boardState[buttonId]) {
+            boardState[buttonId] = false;
+            button.removeClass('bingoButtonCheck');
+            rowCount[row]--;
+            colCount[col]--;
+            if (row === col) diag1Count--;
+            if (row + col === 3) diag2Count--;
+        } else {
+            boardState[buttonId] = true;
+            button.addClass('bingoButtonCheck');
+            rowCount[row]++;
+            colCount[col]++;
+            if (row === col) diag1Count++;
+            if (row + col === 3) diag2Count++;
+        }
+        
+        // Check for bingo
+        if (rowCount[row] === 4 || colCount[col] === 4 || diag1Count === 4 || diag2Count === 4) {
+            handleWin();
+        }
+    }
+
+    function handleWin() {
+        alert('You win!');
+        $('.bingoButton').each(function() {
+            const buttonId = $(this).attr('id');
+            const [row, col] = buttonId.slice(2).split('').map(Number);
+            if (rowCount[row] === 4 || colCount[col] === 4 || (row === col && diag1Count === 4) || (row + col === 3 && diag2Count === 4)) {
+                $(this).addClass('bingoButtonBlink');
+            }
+        });
+    }
+
+
+    $('.bingoButton').click(function() {
+        const buttonId = $(this).attr('id');
+        updateGameState(buttonId);
+    });
+
+    $('.resetButton').click(function() {
+        setupCards();
+    });
 });
