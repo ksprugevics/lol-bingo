93e1565c29562b5c7fb9dceadd44f23726e33a1e
klavssprugevics <klavs.sprugevics@gmail.com>
2023-08-27 10:46:26 +0300

    Python script for converting csv file to JS array

--- bingo.js
+++ bingo.js
@@ -20,6 +20,26 @@ $(document).ready(function() {
         "Accusing someone of feeding",
         "Complaining about an op champ",
         "Complaining about an op item",
+        "Complaining about a summoner spell"
+    ];
+    
+    const aramCards = [
+        "Someone gets a pentakill",
+        "Racial slurs",
+        "GG EZ",
+        "Something in polish",
+        "Level 7 mastery",
+        "100k+ mastery points",
+        "Everyone in a team is Level 5+ mastery",
+        "Everyone has a skin",
+        "Skin from the newest skin-line",
+        "Yasuo/Yone level 7",
+        "Threatening to AFK",
+        "'Dont worry guys, I can carry'",
+        "'Lets FF' / FF15",
+        "Accusing someone of feeding",
+        "Complaining about an op champ",
+        "Complaining about an op item",
         "Poro explodes in ARAM",
         "Complaining about a summoner spell"
     ];
---
+++ resources/convert_cards_to_js_array.py
@@ -0,0 +1,38 @@
+import csv
+
+# Open the CSV file for reading
+csv_filename = 'resources/cards.csv'
+js_filename = 'resources/cards.js'
+
+rift_cards = []
+aram_cards = []
+
+with open(csv_filename, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    
+    # Skip the header row
+    next(csv_reader)
+    
+    for row in csv_reader:
+        name = row[0].strip('"')
+        value = int(row[1])
+
+        if value in [0, 1]:
+            rift_cards.append(name)
+
+        if value in [0, 2]:
+            aram_cards.append(name)
+
+# Create the JavaScript arrays in a text file
+with open(js_filename, 'w') as js_file:
+    js_file.write('const riftCards = [\n')
+    js_file.write(',\n'.join([f'    "{name}"' for name in rift_cards]))
+    js_file.write('\n];\n')
+
+    js_file.write('\n')
+
+    js_file.write('const aramCards = [\n')
+    js_file.write(',\n'.join([f'    "{name}"' for name in aram_cards]))
+    js_file.write('\n];')
+
+print(f'JavaScript arrays written to {js_filename}')
